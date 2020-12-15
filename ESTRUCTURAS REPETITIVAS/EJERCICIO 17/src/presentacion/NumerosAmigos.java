09e72652a4543571cf13379705ea5712270acffd
16george <peraldonamoc@gmail.com>
2020-12-15 15:05:57 -0500

    ejercicios 8 9 12 y 17
    george :)

---
+++ ESTRUCTURAS REPETITIVAS/EJERCICIO 17/src/presentacion/NumerosAmigos.java
@@ -0,0 +1,57 @@
+/*
+ * Dos números son amigos cuando la suma de los divisores de uno de ellos es igual al otro y
+ * viceversa. Muestre, si existen, los números amigos donde ambos números sean menores que N.
+ * Por ejemplo: (220,284), (1184,1210), (2620,2924), (5020,5564), (6232,6368)
+ */
+package presentacion;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Karen
+ */
+public class NumerosAmigos {
+    
+    public static int ingresoDato(){
+        int n;
+        n=Integer.parseInt(JOptionPane.showInputDialog("    Numeros Amigos\n¿Que son los numeros amigos?\nLos numeros amigos son 2 numeros el cual la suma de los divisores de uno de ellos es igual al otro y viceversa.\n\nCalcular los numeros amigos menores que n \n\n     Digite el valor de n"));
+        return n;
+    }
+    
+    public static void numerosAmigos(int n){
+        int i,i_2,i_3,i_4,a,b,c=0,d=0;String resultado="Los Numeros amigos menores que "+n+" son: \n";
+        for(i=1;i<n;i++){
+            a=0;
+            for(i_2=1;i_2<i;i_2++){
+                if(i%i_2==0){
+                    a+=i_2;
+                }
+            }
+            for(i_3=1;i_3<n;i_3++){
+                b=0;
+                for (i_4=1;i_4<i_3;i_4++){
+                    if(i_3%i_4==0){
+                        b+=i_4;
+                    }
+                }
+                if(a==i_3 && b==i){
+                    if(i!=d && i_3!=c){
+                        resultado+=String.valueOf(i)+" y "+String.valueOf(i_3)+"\n";
+                        c=i;d=i_3;
+                    }
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(null,resultado,"Numeros amigos",1);
+    }
+    
+    public static void main(String[] args) {
+       
+        int rt;
+        do{
+        int n=ingresoDato();
+            numerosAmigos(n);
+        rt=JOptionPane.showConfirmDialog(null,"¿Desea hacer otra operacion?\n   Si / NO");
+        
+        }while(rt!=1);
+    }
+}
---
+++ ESTRUCTURAS REPETITIVAS/EJERCICIO 9/src/presentacion/DivisoresNumero.java
@@ -0,0 +1,39 @@
+
+package presentacion;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Kate
+ */
+public class DivisoresNumero {
+    
+    public static int ingresoDato(){
+        int num;
+        do{
+            num=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite un numero que se encuentre en el intervalo <0,3000]","INGRESO DE DATOS",3));
+            if(num<=0 || num>3000){
+                JOptionPane.showMessageDialog(null,"El numero ingresado: "+num+" es incorrepto.\n\nPorfavor vuelva a intentarlo","ADVERTENCIA",2);
+            }
+        }while(num<=0 || num>3000);
+        return num;
+    }
+    public static void divisores(){
+        String resultado="";int numero;numero=ingresoDato();
+        int i;
+        for(i=1;i<=numero;i++){
+            
+            if (numero%i==0) {
+                resultado+=i+"\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null,"Los divisores del numero "+numero+" son \n"+resultado,"DIVISORES",1);
+    }
+    public static void main(String[] args) {
+        int rt;
+        do{
+        divisores();
+        rt=JOptionPane.showConfirmDialog(null,"¿Desea hacer otra operacion?\n   Si / NO");
+        
+        }while(rt!=1);
+    }
+}
